b4a772a711c44c7aadbaca40bf0ca3846e2c59b7
Max Beier <max@maxbeier.dev>
2023-01-08 22:26:03 +0100

    Update init.py

--- init.py
+++ init.py
@@ -9,14 +9,10 @@ STEPSIZE = 1
 controller = Controller()
 
 def main():
-        
-        p = [10, 30, 40]
-        
-        controller.moveTo(p)
-        
-        #controller.move("Base" , random.randint(0,180), SPEED, STEPSIZE)
-        #controller.move("Axis1", random.randint(0,180), SPEED, STEPSIZE)
-        #controller.move("Axis2", random.randint(0,180), SPEED, STEPSIZE)
-        #controller.move("Catch", random.randint(0,180), SPEED, STEPSIZE)
+        controller.move("Base" , random.randint(0,180), SPEED, STEPSIZE)
+        controller.move("Axis1", random.randint(0,180), SPEED, STEPSIZE)
+        controller.move("Axis2", random.randint(0,180), SPEED, STEPSIZE)
+        controller.move("Catch", random.randint(0,180), SPEED, STEPSIZE)
 
+        
 main()
